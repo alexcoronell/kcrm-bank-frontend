3ecced3792a8eff1c75c144fe541d6d7f06fbd08
Alex Coronell <alexcoronell@gmail.com>
2024-10-22 11:16:41 -0500

    Header and footer component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 function App() {
-  return <h1>KCRM Bank</h1>;
+  return (
+    <>
+      <Header />
+      <h1>KCRM Bank</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+    return (<h4 className="Header">Header</h4>)
+};
