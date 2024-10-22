023343cdb4f32ba304f45cd8c79078e7de65bd62
Alex Coronell <alexcoronell@gmail.com>
2024-10-22 13:17:28 -0500

    Footer completed

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,3 +1,13 @@
+import Logo from "../ui/Logo"
+
+
 export default function Footer() {
-    return (<h4 className="Footer">Footer</h4>)
+    const date = new Date();
+    const year = date.getFullYear()
+    return (<footer className="Footer w-full border-t-2">
+        <div className="w-full max-w-[1200px] mx-auto flex items-center justify-between py-6">
+            <Logo />
+            <h5 className="text-lg">Copyright © {year} Grupo KCRM</h5>
+        </div>
+    </footer>)
 };
