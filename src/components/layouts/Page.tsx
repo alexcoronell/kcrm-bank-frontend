89180cb60a5762fe5baf1307acbfa9f41ea323a7
Alex Coronell <alexcoronell@gmail.com>
2024-10-22 19:58:42 -0500

    Create button added on page layout

--- src/components/layouts/Page.tsx
+++ src/components/layouts/Page.tsx
@@ -1,12 +1,27 @@
+import { Link } from "wouter";
+import { Button } from "../ui/Button";
+
 interface Pageprops {
   titlePage: string;
+  titleButton?: string | null;
+  urlButton?: string | null;
   children: React.ReactNode;
 }
 
-export default function Page({ titlePage, children }: Pageprops) {
+export default function Page({
+  titlePage,
+  titleButton,
+  urlButton,
+  children,
+}: Pageprops) {
   return (
     <div className="Page w-full max-w-[1400px] grow py-6">
       <h3 className="py-10 text-center text-4xl">{titlePage}</h3>
+      {titleButton && urlButton ? (
+        <Link href={urlButton}>
+          <Button className="mb-6">{titleButton}</Button>
+        </Link>
+      ) : null}
       <main>{children}</main>
     </div>
   );
--- src/pages/FranchisesPage.tsx
+++ src/pages/FranchisesPage.tsx
@@ -1,10 +1,12 @@
-import Page from "../components/layouts/Page"
+import Page from "../components/layouts/Page";
 import { FranchisesTable } from "../components/Shared/FranchisesTable";
+const titleButton = "Nueva franquicia"
+const url = "/franchises/create"
 
 export default function FranchisesPage() {
-    return (
-        <Page titlePage="Franquicias">
-          <FranchisesTable />
-        </Page>
-      );
-};
+  return (
+    <Page titlePage="Franquicias" titleButton={titleButton} urlButton={url}>
+      <FranchisesTable />
+    </Page>
+  );
+}
