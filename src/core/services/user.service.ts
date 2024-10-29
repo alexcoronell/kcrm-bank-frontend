0bb81cf4c29d71c0cfee58bddbaad5c96b983272
Alex Coronell <alexcoronell@gmail.com>
2024-10-29 15:39:37 -0500

    User service completed

--- src/core/services/user.service.ts
+++ src/core/services/user.service.ts
@@ -1,12 +1,39 @@
+import axios from "axios";
+
 const url = `${import.meta.env.VITE_API_URL}/users`;
 
+/* Interfaces */
+import { User } from "../interfaces/User.interface";
+
+/* DTO's */
+import { CreateUserDto, UpdateUserDto } from "../dtos/User.dto";
+
 export default class UserService {
-  static getAll = async () => {
-    try {
-      const data = await fetch(url);
-      return await data.json();
-    } catch (e) {
-      console.error(e);
-    }
+  static save = async (dto: CreateUserDto) => {
+    return await axios.post(url, dto, {
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
+  static getAll = async (page = 1, limit = 10) => {
+    return await axios.get(`${url}?page=${page}&limit=${limit}`);
+  };
+
+  static count = async () => {
+    return await axios.get(`${url}/count`);
+  };
+
+  static get = async (id: User["id"]) => {
+    return await axios.get(`${url}/${id}`);
+  };
+
+  static update = async (id: User["id"], dto: UpdateUserDto) => {
+    return await axios.put(`${url}/${id}`, dto, {
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
+  static delete = async (id: User["id"]) => {
+    return await axios.delete(`${url}/${id}`);
   };
 }
