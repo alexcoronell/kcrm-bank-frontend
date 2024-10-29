39c345a19e235de45933826172998e343c2d59c7
Alex Coronell <alexcoronell@gmail.com>
2024-10-29 15:28:40 -0500

    User dto created and completed

---
+++ src/core/dtos/User.dto.ts
@@ -0,0 +1,17 @@
+import { User } from "../interfaces/User.interface";
+
+export interface CreateUserDto
+  extends Omit<
+    User,
+    "id" | "createAt" | "updateAt" | "active" | "deleted" | "userType"
+  > {
+  userType: number;
+}
+
+export interface UpdateUserDto extends CreateUserDto {
+  active: boolean;
+}
+
+export interface UpdateUserPasswordDto {
+  password: string;
+}
