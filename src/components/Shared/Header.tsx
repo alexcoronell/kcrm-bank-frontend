9f590e3e4ff9a6455a8b9a377ab1972d138953bc
Alex Coronell <alexcoronell@gmail.com>
2024-10-31 15:07:22 -0500

    Login button added on header

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,11 +1,17 @@
+import { Link } from "wouter";
 import { Button } from "../ui/Button";
 import Logo from "../ui/Logo";
 
 export default function Header() {
-	return (
-		<header className="Header flex p-2 items-center justify-between w-full shadow-md">
-			<Logo />
-			<Button variant="light">Logout</Button>
-		</header>
-	);
+  return (
+    <header className="Header flex p-2 items-center justify-between w-full shadow-md">
+      <Logo />
+      <div>
+        <Link href="/login">
+          <Button variant="primary">Iniciar sesión</Button>
+        </Link>
+        <Button variant="light">Cerrar sesión</Button>
+      </div>
+    </header>
+  );
 }
