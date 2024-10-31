4216f78d9103c4e886e8b4eb64a95d98ab9c247c
Alex Coronell <alexcoronell@gmail.com>
2024-10-30 21:21:09 -0500

    Back button added on page component

--- src/components/layouts/Page.tsx
+++ src/components/layouts/Page.tsx
@@ -2,27 +2,32 @@ import { Link } from "wouter";
 import { Button } from "../ui/Button";
 
 interface Pageprops {
-	titlePage: string;
-	titleButton?: string | null;
-	urlButton?: string | null;
-	children: React.ReactNode;
+  titlePage: string;
+  titleButton?: string | null;
+  urlButton?: string | null;
+  children: React.ReactNode;
 }
 
 export default function Page({
-	titlePage,
-	titleButton,
-	urlButton,
-	children,
+  titlePage,
+  titleButton,
+  urlButton,
+  children,
 }: Pageprops) {
-	return (
-		<div className="Page w-full max-w-[1400px] grow py-6">
-			<h3 className="py-10 text-center text-4xl">{titlePage}</h3>
-			{titleButton && urlButton ? (
-				<Link href={urlButton}>
-					<Button className="mb-6">{titleButton}</Button>
-				</Link>
-			) : null}
-			<main>{children}</main>
-		</div>
-	);
+  return (
+    <div className="Page w-full max-w-[1400px] grow py-6">
+      <h3 className="py-10 text-center text-4xl">{titlePage}</h3>
+      {titleButton && urlButton ? (
+        <div className="tableButtons flex gap-x-3">
+          <Link href="/dashboard">
+            <Button variant="secondary">Regresar</Button>
+          </Link>
+          <Link href={urlButton}>
+            <Button className="mb-6">{titleButton}</Button>
+          </Link>
+        </div>
+      ) : null}
+      <main>{children}</main>
+    </div>
+  );
 }
