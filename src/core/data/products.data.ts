c6e0584397ae59d0c5c22cf27d94fef2042ada45
Alex Coronell <alexcoronell@gmail.com>
2024-10-30 19:15:59 -0500

    Product enum and data removed

--- src/core/data/products.data.ts
+++
@@ -1,7 +0,0 @@
-import type { Product } from "../interfaces/Product.interface";
-
-export const products: Product[] = [
-	{ name: "Crédito de Libre Consumo" },
-	{ name: "Libranza Libre Inversión" },
-	{ name: "Tarjeta de Crédito" },
-];
--- src/core/enums/Product.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum Product {
-	"Crédito de Libre Consumo" = 0,
-	"Libranza Libre Inversión" = 1,
-	"Tarjeta de Crédito" = 2,
-}
