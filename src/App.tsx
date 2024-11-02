4c58483e95e0cb5ee874affc112b0ff387786a0a
Alex Coronell <alexcoronell@gmail.com>
2024-11-01 21:58:51 -0500

    AppProvider applied on App.txs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Router } from "wouter";
 
-import Footer from "./components/Shared/Footer";
+import { AppProvider } from "./context";
+
 /* Components */
 import Header from "./components/Shared/Header";
-
 import FranchisesFormPage from "./pages/private/FranchisesFormPage";
+import Footer from "./components/Shared/Footer";
+
 /* Pages */
 import DashboardPage from "./pages/private/DashboardPage";
 import FranchisesPage from "./pages/private/FranchisesPage";
@@ -19,35 +21,38 @@ import RolesPage from "./pages/private/RolesPage";
 import UsersFormPage from "./pages/private/UsersFormPage";
 import UsersPage from "./pages/private/UsersPage";
 
+/* Styles */
 import "./App.css";
 
 function App() {
-  return (
-    <div className="min-h-screen p-0 flex flex-col items-center justify-between">
-      <Header />
-      <Router>
-        <Route path="/" component={MainPage} />
-        <Route path="/dashboard" component={DashboardPage} />
-        <Route path="/franchises" component={FranchisesPage} />
-        <Route path="/franchises/create" component={FranchisesFormPage} />
-        <Route path="/franchises/detail/:id" component={FranchisesFormPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/products" component={ProductsPage} />
-        <Route path="/products/create" component={ProductsFormPage} />
-        <Route path="/products/detail/:id" component={ProductsFormPage} />
-        <Route path="/sales" component={SalesPage} />
-        <Route path="/sales/create" component={SalesFormPage} />
-        <Route path="/sales/detail/:ID" component={SalesFormPage} />
-        <Route path="/users" component={UsersPage} />
-        <Route path="/users/create" component={UsersFormPage} />
-        <Route path="/users/detail/:id" component={UsersFormPage} />
-        <Route path="/roles" component={RolesPage} />
-        <Route path="/roles/create" component={RolesFormPage} />
-        <Route path="/roles/detail/:id" component={RolesFormPage} />
-      </Router>
-      <Footer />
-    </div>
-  );
+	return (
+		<AppProvider>
+			<div className="min-h-screen p-0 flex flex-col items-center justify-between">
+				<Header />
+				<Router>
+					<Route path="/" component={MainPage} />
+					<Route path="/dashboard" component={DashboardPage} />
+					<Route path="/franchises" component={FranchisesPage} />
+					<Route path="/franchises/create" component={FranchisesFormPage} />
+					<Route path="/franchises/detail/:id" component={FranchisesFormPage} />
+					<Route path="/login" component={LoginPage} />
+					<Route path="/products" component={ProductsPage} />
+					<Route path="/products/create" component={ProductsFormPage} />
+					<Route path="/products/detail/:id" component={ProductsFormPage} />
+					<Route path="/sales" component={SalesPage} />
+					<Route path="/sales/create" component={SalesFormPage} />
+					<Route path="/sales/detail/:ID" component={SalesFormPage} />
+					<Route path="/users" component={UsersPage} />
+					<Route path="/users/create" component={UsersFormPage} />
+					<Route path="/users/detail/:id" component={UsersFormPage} />
+					<Route path="/roles" component={RolesPage} />
+					<Route path="/roles/create" component={RolesFormPage} />
+					<Route path="/roles/detail/:id" component={RolesFormPage} />
+				</Router>
+				<Footer />
+			</div>
+		</AppProvider>
+	);
 }
 
 export default App;
