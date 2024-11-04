163ff3e68d07ea3ba61b39e54b3f84f9de4ee4f2
Alex Coronell <alexcoronell@gmail.com>
2024-11-04 13:37:06 -0500

    Menu conditional admin and not admin

--- src/components/Shared/Menu.tsx
+++ src/components/Shared/Menu.tsx
@@ -1,35 +1,44 @@
+import { useContext } from "react";
 import { Link } from "wouter";
+
+import { AppContext } from "../../context";
 
 import { Button } from "../ui/Button";
 
 export default function Menu() {
-  return (
-    <nav className="Menu flex flex-col gap-y-1 h-full">
-      <Link href="/franchises">
-        <Button className="w-[200px]" variant="secondary">
-          Franquicias
-        </Button>
-      </Link>
-      <Link href="/products">
-        <Button className="w-[200px]" variant="secondary">
-          Productos
-        </Button>
-      </Link>
-      <Link href="/sales">
-        <Button className="w-[200px]" variant="secondary">
-          Ventas
-        </Button>
-      </Link>
-      <Link href="/users">
-        <Button className="w-[200px]" variant="secondary">
-          Usuarios
-        </Button>
-      </Link>
-      <Link href="/roles">
-        <Button className="w-[200px]" variant="secondary">
-          Roles
-        </Button>
-      </Link>
-    </nav>
-  );
+	const context = useContext(AppContext);
+
+	return (
+		<nav className="Menu flex flex-col gap-y-1 h-full">
+			<Link href="/franchises">
+				<Button className="w-[200px]" variant="secondary">
+					Franquicias
+				</Button>
+			</Link>
+			<Link href="/products">
+				<Button className="w-[200px]" variant="secondary">
+					Productos
+				</Button>
+			</Link>
+			<Link href="/sales">
+				<Button className="w-[200px]" variant="secondary">
+					Ventas
+				</Button>
+			</Link>
+			{context.isAdmin && (
+				<>
+					<Link href="/users">
+						<Button className="w-[200px]" variant="secondary">
+							Usuarios
+						</Button>
+					</Link>
+					<Link href="/roles">
+						<Button className="w-[200px]" variant="secondary">
+							Roles
+						</Button>
+					</Link>
+				</>
+			)}
+		</nav>
+	);
 }
