fb91edf0e773bcd581c3a809ec7ba549c46e35f6
Alex Coronell <alexcoronell@gmail.com>
2024-11-08 08:52:46 -0500

    SwitchGroup Components created

---
+++ src/components/Shared/SwitchGroup.tsx
@@ -0,0 +1,32 @@
+import { Switch } from "../ui/Switch";
+import { Label } from "../ui/Label";
+
+interface Props {
+  label: string;
+  name: string;
+  checked: boolean;
+  onCheckedChange: () => void;
+  disabled: boolean;
+}
+
+export default function SwitchGroup({
+  label,
+  name,
+  checked = false,
+  onCheckedChange,
+  disabled = false,
+}: Props) {
+  return (
+    <div className="flex items-center justify-center gap-2 my-6 md:py-3">
+      {" "}
+      <Switch
+        id={name}
+        name={name}
+        checked={checked}
+        onCheckedChange={onCheckedChange}
+        disabled={disabled}
+      />{" "}
+      <Label htmlFor="activateUser">{label}</Label>{" "}
+    </div>
+  );
+}
